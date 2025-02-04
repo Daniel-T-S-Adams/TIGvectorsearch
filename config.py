b7ea4175897968d37f4cc7076e2dcfcdad38e829
Daniel Adams <santostiagodaniel@gmail.com>
2025-02-04 22:15:11 +0000

    small edit

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Configuration parameters for vector search algorithms
 VECTOR_SEARCH_CONFIG = {
     'n_database': 100000,
-    'n_queries': 600,
+    'n_queries': 1000,
     'dimension': 250,
-    'threshold': 5.8,  # Increased threshold to account for high dimensionality
+    'threshold': 5.9,  # Increased threshold to account for high dimensionality
     'seed': 1,
     'n_seeds': 100  # Number of different random seeds to use for experiments
 } 
